792c0ece8685f71b3d1901a27ad76e6f1ac558f3
Lucas Cantarelli Lustosa <163223802+cantalusto@users.noreply.github.com>
2024-10-22 13:20:41 -0300

    RESIDENCIA GIT

---
+++ src/components/Home.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './Home.css';
+import Sidebar from './Sidebar';
+
+const Home = () => {
+  return (
+    <div className="container">
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Main content */}
+      <main className="content">
+        <header>
+          <nav>
+            <ul className="top-menu">
+              <li><a href="#">Mural</a></li>
+              <li><a href="#">Atividades</a></li>
+              <li><a href="#">Notas</a></li>
+            </ul>
+            <div className="config-icon">
+              <img src="config-icon.png" alt="Configurações" />
+            </div>
+          </nav>
+        </header>
+
+        {/* Atividades */}
+        <section className="atividades">
+          <div className="atividades-pendentes">
+            <h2>Atividades Pendentes</h2>
+            <div className="atividade-item">
+              <span className="icon">&#x1F4DD;</span>
+              <div className="detalhes">
+                <h3>Resistores e associação de resistores</h3>
+                <p>Data de entrega: 25 de Set. de 2024, 10:00</p>
+              </div>
+              <span className="seta">&#x27A4;</span>
+            </div>
+            {/* Adicionar mais itens conforme necessário */}
+          </div>
+
+          <div className="atividades-respondidas">
+            <h2>Atividades Respondidas</h2>
+            <div className="atividade-item">
+              <span className="icon">&#x1F7E2;</span>
+              <div className="detalhes">
+                <h3>Nome da Atividade</h3>
+              </div>
+              <span className="seta">&#x27A4;</span>
+            </div>
+            {/* Adicionar mais itens conforme necessário */}
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <aside className="sidebar">
+      <div className="logo">
+        <img src="logo.png" alt="Logo" />
+      </div>
+      <nav className="menu">
+        <ul>
+          <li><a href="#">Turmas</a></li>
+          <li><a href="#">Atividades</a></li>
+          <li><a href="#">Configurações</a></li>
+        </ul>
+      </nav>
+    </aside>
+  );
+};
+
+export default Sidebar;
