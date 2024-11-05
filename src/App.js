d2d95fd5a9a68fd4fd3abd9d127fed3d72b4b147
isa <157431232+marialuisamatos@users.noreply.github.com>
2024-11-05 20:48:12 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/login/login.js";
-import DashAluno from "./pages/DashAluno/AlunoPage.js";
-import DashProf from "./pages/DashProf/ProfessorPage.js";
-import ResponderAtividade from './pages/Responder/ResponderAtividade';
-import Grades from "./pages/Grades/Grades.js";
-import CadastroAtividades from "./pages/CadastroAtividades/CadastroAtividades.js";
+import React from 'react';
+import './App.css';
+import menu from './img/menu.png';
+import inicio from './img/inicio.png';
+import turma from './img/turma.png';
+import atividades from './img/atividades.png';
+import configuracoes from './img/configuracoes.png';
+import logoFeras from './img/logoferas.png';
+import notificacaoImg from './img/notificação.png';
+import atividadeBrancoImg from './img/atividadebranco.png';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/aluno" element={<DashAluno />} />
-        <Route path="/professor" element={<DashProf />} />
-        <Route path="/responder-atividade" element={<ResponderAtividade />} />
-        <Route path="/grades" element={<Grades />} />
-        <Route path="/cadastro" element={<CadastroAtividades />} />
-      </Routes>
-    </Router>
+    <div className="container">
+      <nav className="menu">
+        <ul>
+          <div className="menu-inicial">
+            <img src={menu} alt="menu-inicial" />
+          </div>
+          <li>
+            <div className="navbar-item">
+              <img src={inicio} alt="inicio" />
+              <span>Início</span>
+            </div>
+          </li>
+          <li>
+            <div className="navbar-item">
+              <img src={turma} alt="turma" />
+              <span>Turmas</span>
+            </div>
+          </li>
+          <li>
+            <div className="navbar-item">
+              <img src={atividades} alt="atividades" />
+              <span className="borda">Atividades</span>
+            </div>
+          </li>
+          <li>
+            <div className="navbar-item">
+              <img src={configuracoes} alt="configurações" />
+              <span>Configurações</span>
+            </div>
+          </li>
+        </ul>
+      </nav>
+      <div className="main-content">
+        <div className="mural">
+          <nav>
+            <img className="logoFeras" src={logoFeras} alt="logo" />
+            <ul className="centralizado">
+              <li>Mural</li>
+              <li className="borda">Atividades</li>
+              <li className="notificacao">Notas</li>
+              <img className="notificacao" src={notificacaoImg} alt="notificação" />
+            </ul>
+          </nav>
+          <hr className="cabecario" />
+        </div>
+        <div className="section">
+          <div className="principal">
+            <header>
+              <div className="header-content">
+                <img src={atividadeBrancoImg} alt="atividadesamarela" className="header-image" />
+                <h1>Resistores e associação de resistores</h1>
+              </div>
+              <p>Professor: Nome do Professor</p>
+              <div className="descricao">
+                <p style={{ display: 'inline-block', marginRight: '30%' }}>01. Cálculos de Resistência</p>
+                <p style={{ display: 'inline-block' }}>Data de Entrega: 26 de Set, de 2024, 10:00</p>
+                <p style={{ display: 'inline-block', marginRight: '31%' }}>02. Montagem de Circuitos</p>
+                <p style={{ display: 'inline-block' }}>Data de Entrega: 01 de Out, de 2024, 12:00</p>
+                <p style={{ display: 'inline-block', marginRight: '31%' }}>03. Simulações e Software</p>
+                <p style={{ display: 'inline-block' }}>Data de Entrega: 10 de Out, de 2024, 13:00</p>
+              </div>
+            </header>
+          </div>
+        </div>
+        <footer>@direitos reservados feras</footer>
+      </div>
+    </div>
   );
 };
 
