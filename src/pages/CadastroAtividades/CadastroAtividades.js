47d269f1185caeef964603d0f2cf96c1a15b1249
thiagoduarte1987 <thiago.duarte1987@gmail.com>
2024-11-05 21:24:29 -0300

    Inclusão do botão voltar na tela de cadastro de atividade

--- src/pages/CadastroAtividades/CadastroAtividades.js
+++ src/pages/CadastroAtividades/CadastroAtividades.js
@@ -1,12 +1,18 @@
 // src/components/CadastroAtividades.js
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './CadastroAtividades.css';
 import Sidebar from "../../components/Sidebar/Sidebarprof.js";
 
 function CadastroAtividades() {
-    
+    const navigate = useNavigate();
     const [titulo, setTitulo] = useState(''); // Armazena o título da atividade
     const [questoes, setQuestoes] = useState(['']); // Armazena as questões, começa com uma questão vazia
+
+    // Função para voltar à página anterior
+    const handleBack = () => {
+        navigate(-1);
+    };
 
     // Função para adicionar uma nova questão
     const adicionarQuestao = () => {
@@ -54,39 +60,38 @@ function CadastroAtividades() {
     };
 
     return (
-
         <div className="container">
             <Sidebar />
             <div className="content">
-            <h2>Cadastrar Atividade</h2>
-            <form onSubmit={handleSubmit}>
-                <label>Título da Atividade:</label>
-                <input
-                    type="text"
-                    value={titulo}
-                    onChange={(e) => setTitulo(e.target.value)}
-                    required
-                />
+            <button onClick={handleBack} className="voltar">Voltar</button>
+                <h2>Cadastrar Atividade</h2>
+                <form onSubmit={handleSubmit}>
+                    <label>Título da Atividade:</label>
+                    <input
+                        type="text"
+                        value={titulo}
+                        onChange={(e) => setTitulo(e.target.value)}
+                        required
+                    />
 
-                <h3>Questões</h3>
-                {questoes.map((questao, index) => (
-                    <div key={index} className="questao-container">
-                        <input
-                            type="text"
-                            value={questao}
-                            onChange={(e) => atualizarQuestao(index, e.target.value)}
-                            placeholder={`Questão ${index + 1}`}
-                            required
-                        />
-                        <button type="button" onClick={() => removerQuestao(index)}>Remover</button>
-                    </div>
-                    
-                ))}
-                <button type="button" onClick={adicionarQuestao}>Adicionar Questão</button>
+                    <h3>Questões</h3>
+                    {questoes.map((questao, index) => (
+                        <div key={index} className="questao-container">
+                            <input
+                                type="text"
+                                value={questao}
+                                onChange={(e) => atualizarQuestao(index, e.target.value)}
+                                placeholder={`Questão ${index + 1}`}
+                                required
+                            />
+                            <button type="button" onClick={() => removerQuestao(index)}>Remover</button>
+                        </div>
+                    ))}
+                    <button type="button" onClick={adicionarQuestao}>Adicionar Questão</button>
 
-                <button type="submit">Cadastrar</button>
-            </form>
-        </div>
+                    <button type="submit">Cadastrar</button>
+                </form>
+            </div>
         </div>
     );
 }
